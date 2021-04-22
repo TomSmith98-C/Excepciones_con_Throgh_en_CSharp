303416c8c7c4af37a1c6c25124f22b0e155d07ae
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-22 13:37:33 -0500

    Add files via upload

---
+++ ExceptionThrogh/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionThrogh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digita un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            try
+            {
+                cod(numero);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error"+e.Message);
+            }
+        }
+        static void cod(int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Digito: 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Digito: 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Digito: 3");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
